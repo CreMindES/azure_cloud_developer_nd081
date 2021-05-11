16a11474aceaa3cb1259320b31d5041971fcb127
Tamás G.Barna <cremindes@gmail.com>
2021-05-11 22:40:05 +0000

    Minimal chi based server with endpoint placeholders.

--- part-2-azure-applications/connect-app-to-storage-starter-go/main.go
+++ part-2-azure-applications/connect-app-to-storage-starter-go/main.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
 	config := NewConfig()
 
-	fmt.Println(config)
+	imageSourceUrl, err := url.Parse("https://"+ config.blobAccount  + ".blob.core.windows.net/" +
+		config.blobContainer  + "/")
+	if err != nil {
+		log.Fatalf("Invalid image source URL | %v", err)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", home)
+	r.Get("/home", home)
+
+	r.Get("/animal", animal)
+
+	fmt.Println(imageSourceUrl)
+
+	serverErr := http.ListenAndServe(":"+strconv.Itoa(5555), r)
+
+	if serverErr != nil {
+		log.Fatalf("server failed: %v", serverErr)
+	}
+}
+
+// home home page.
+func home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// animal animal page.
+func animal(w http.ResponseWriter, r *http.Request) {
+
 }
