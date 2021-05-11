3ab188738635091b860679a23cf72b8df8e00535
Tamás G.Barna <cremindes@gmail.com>
2021-05-11 22:26:49 +0000

    Config representation.

--- part-2-azure-applications/connect-app-to-storage-starter-go/config.go
+++ part-2-azure-applications/connect-app-to-storage-starter-go/config.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/url"
+import (
+	"log"
+	"net/url"
+	"os"
+)
 
 // Config holds all the configuration variables that are needed for this app.
 // Structure is direct copy from original Udacity, Python implementation.
@@ -10,9 +14,34 @@ type Config struct {
 	SQLDatabase                  string
 	SQLUsername                  string
 	SQLPassword                  string
-	SQLAlchemyDatabaseURI        url.URL
+	SQLAlchemyDatabaseURI        *url.URL
 	SQLAlchemyTrackModifications bool
 	blobAccount                  string
 	blobStorageKey               string
 	blobContainer                string
+}
+
+func NewConfig() Config {
+	config := Config{
+		secretKey:                    os.Getenv("SECRET_KEY"),
+		SQLDatabase:                  os.Getenv("SQL_DATABASE"),
+		SQLPassword:                  os.Getenv("SQL_PASSWORD"),
+		SQLUsername:                  os.Getenv("SQL_SERVER"),
+		SQLServer:                    os.Getenv("SQL_USER_NAME"),
+		SQLAlchemyTrackModifications: false,
+		blobAccount:                  os.Getenv("BLOB_ACCOUNT"),
+		blobContainer:                os.Getenv("BLOB_CONTAINER"),
+		blobStorageKey:               os.Getenv("BLOB_STORAGE_KEY"),
+	}
+
+	var err error
+	config.SQLAlchemyDatabaseURI, err = url.Parse("mssql+pyodbc://" +
+		config.SQLUsername + "@" + config.SQLServer + ":" + config.SQLPassword 	+ "@" +
+		config.SQLServer + ":1433/" + config.SQLDatabase + "?driver=ODBC+Driver+17+for+SQL+Server")
+
+	if err != nil {
+		log.Fatalf("Invalid SQLAlchemyDatabaseURI | %v", err)
+	}
+
+	return config
 }
